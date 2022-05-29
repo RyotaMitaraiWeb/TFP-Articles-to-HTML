ac8734500da6ef636b5a8d3a7fee96de51753a45
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-05-29 16:45:33 +0300

    add parseParagrpahs module, add skeleton for article structure, and add sample team formatter

---
+++ js/modules/addParagraphs.js
@@ -0,0 +1,20 @@
+export function addParagraphs(article) {
+    article = article.replace(/^(?=[a-z0-9])/gmi, '<p>');
+    const paragraph = article.match(/^<p>/gmi);
+    if (paragraph !== null) {
+        const last = paragraph.length - 1;
+        for (let count = 0; count <= last; count++) {
+            article = article.replace(/^\<p\>/mi, `${count}<p>`);
+        }
+        for (let count = 0; count <= last; count++) {
+            if (count === last) {
+                article = article.replace(RegExp(`^${count}<p>`, 'mi'), '<p style="margin-bottom: 1em;">');
+            }
+            else {
+                article = article.replace(RegExp(`^${count}<p>`, 'mi'), '<p>');
+            }
+        }
+    }
+    return article.replace(/(?<=<p.*?>.*?)$/gmi, '</p>');
+}
+//# sourceMappingURL=addParagraphs.js.map
