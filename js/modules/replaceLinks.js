24093444e7434ffbc34e46bf53d9be7bc2b78268
RyotaMitaraiWeb <Titanium5624@gmail.com>
2022-05-31 15:51:23 +0300

    fix bug with hyperlinks whose URL is within the tag itself

--- js/modules/replaceLinks.js
+++ js/modules/replaceLinks.js
@@ -1,18 +1,30 @@
 export function replaceLinks(article) {
-    const links = article.match(/\[URL.*?\].*?\[\/URL\]/gm);
+    const pureLinks = article.match(/\[URL\].*?\[\/URL\]/gm);
+    if (pureLinks !== null) {
+        for (const link of pureLinks) {
+            const urlMatch = link.match(/(?<=\[URL\]).*?(?=\[\/URL\])/gmi);
+            if (urlMatch !== null) {
+                const url = transformURL(urlMatch[0]);
+                article = article.replace(link, `<a href="${url}" target="_blank">${urlMatch[0]}</a>`);
+            }
+        }
+    }
+    const links = article.match(/\[URL=.*?\].*?\[\/URL\]/gm);
     if (links !== null) {
         for (const link of links) {
-            let anchor = link
-                .replace('[URL', '<a href')
-                .replace('=\'', '="')
-                .replace('\']', '" target="_blank">')
-                .replace('[/URL]', '</a>')
-                .replace('https:', '')
-                .replace('http:', '')
-                .replace('//www.smogon.com', '');
+            const anchor = transformURL(link);
             article = article.replace(link, anchor);
         }
     }
     return article;
+}
+function transformURL(url) {
+    return url.replace('[URL', '<a href')
+        .replace('=\'', '="')
+        .replace('\']', '" target="_blank">')
+        .replace('[/URL]', '</a>')
+        .replace('https:', '')
+        .replace('http:', '')
+        .replace('//www.smogon.com', '');
 }
 //# sourceMappingURL=replaceLinks.js.map
--- ts/modules/replaceLinks.ts
+++ ts/modules/replaceLinks.ts
@@ -1,20 +1,34 @@
 export function replaceLinks(article: string): string {
-    const links: RegExpMatchArray | null = article.match(/\[URL.*?\].*?\[\/URL\]/gm);
+    const pureLinks: RegExpMatchArray | null = article.match(/\[URL\].*?\[\/URL\]/gm);
+
+    if (pureLinks !== null) {
+        for (const link of pureLinks) {
+            const urlMatch: RegExpMatchArray | null = link.match(/(?<=\[URL\]).*?(?=\[\/URL\])/gmi);
+            if (urlMatch !== null) {
+                const url: string = transformURL(urlMatch[0]);
+                article = article.replace(link, `<a href="${url}" target="_blank">${urlMatch[0]}</a>`);
+            }
+        }
+    }
+    
+    const links: RegExpMatchArray | null = article.match(/\[URL=.*?\].*?\[\/URL\]/gm);
 
     if (links !== null) {
         for (const link of links) {
-            let anchor: string = link
-            .replace('[URL', '<a href')
-            .replace('=\'', '="')
-            .replace('\']', '" target="_blank">')
-            .replace('[/URL]', '</a>')
-            .replace('https:', '')
-            .replace('http:', '')
-            .replace('//www.smogon.com', '');
-
+            const anchor: string = transformURL(link);
             article = article.replace(link, anchor);
         }
     }
 
     return article;
+}
+
+function transformURL(url: string): string {
+    return url.replace('[URL', '<a href')
+        .replace('=\'', '="')
+        .replace('\']', '" target="_blank">')
+        .replace('[/URL]', '</a>')
+        .replace('https:', '')
+        .replace('http:', '')
+        .replace('//www.smogon.com', '');
 }
